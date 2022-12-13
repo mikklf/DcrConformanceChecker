23bbe348d75ab6a5c02d9e85553404ace2b2b141
Mikkel <4072916+mikk3@users.noreply.github.com>
2022-12-13 13:45:21 +0100

    Add PrintTrace to LogParser

--- LogParser/LogTrace.cs
+++ LogParser/LogTrace.cs
@@ -3,6 +3,7 @@ namespace DcrConformanceChecker.LogParser;
 public class LogTrace {
 
     public readonly List<LogEvent> Trace;
+    public string TraceID => Trace.First().TraceId;
 
     public LogTrace(List<LogEvent> trace)
     {
@@ -10,5 +11,15 @@ public class LogTrace {
 
         // Sort Trace by timestamp ascending
         Trace = Trace.OrderBy(x => x.Timestamp).ToList();
+    }
+
+    public void PrintTrace() {
+
+        Console.Write($"{TraceID} = <");
+
+        foreach (var logEvent in Trace) 
+            Console.Write(logEvent.Activity + ", ");
+        
+        Console.Write(">");
     }
 }
