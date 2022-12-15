f29137ba962373d86068e75ffc38d05679e61559
Mikkel <4072916+Mikk3@users.noreply.github.com>
2022-12-15 10:06:27 +0100

    Change parameter naming in LogTrace

--- Parsers/LogParser/LogTrace.cs
+++ Parsers/LogParser/LogTrace.cs
@@ -3,20 +3,20 @@ namespace DcrConformanceChecker.Parsers.LogParser;
 public class LogTrace
 {
 
-    public readonly List<LogEvent> Trace;
-    public string TraceID => Trace.First().TraceId;
+    public readonly List<LogEvent> Events;
+    public string TraceId => Events.First().TraceId;
 
-    public LogTrace(List<LogEvent> trace)
+    public LogTrace(List<LogEvent> events)
     {
-        Trace = trace;
+        Events = events;
 
         // Sort Trace by timestamp ascending
-        Trace = Trace.OrderBy(x => x.Timestamp).ToList();
+        Events = Events.OrderBy(x => x.Timestamp).ToList();
     }
 
     public void PrintTrace()
     {
-        var trace_str = String.Join(", ", Trace.Select(x => x.Activity));
-        Console.Write($"{TraceID} = <{trace_str}>");
+        var trace_str = String.Join(", ", Events.Select(x => x.Activity));
+        Console.WriteLine($"{TraceId} = <{trace_str}>");
     }
 }
